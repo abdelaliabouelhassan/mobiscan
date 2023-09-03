dfc5864ced7294a587a725b69665e7fe0c3f8553
abdelghafour-99 <abdelghafour.elballaoui@gmail.com>
2023-09-03 16:36:50 +0100

    prj setup

---
+++ components/partials/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="w-full container rounded-b-[2.5rem]  px-8 shadow-[0_4px_4px_0_rgba(0,0,0,0.05)]">
+        <nav class="flex items-center justify-between py-3.5 ">
+            <img class="h-8  object-center" src="/images/logo/logo.svg" alt="" srcset="">
+            <button class="flex justify-center items-center px-6 py-[0.625rem] text-port-gore border border-port-gore rounded-full 
+                tracking-[0.32px] uppercase transition-colors duration-300 ease-in-out
+                hover:bg-port-gore hover:text-white ">
+                MENU
+            </button>
+            <button class="bg-caribbean-green flex justify-center items-center px-6 py-[0.625rem] text-port-gore rounded-full 
+                tracking-[0.32px] uppercase transition-colors duration-300 ease-in-out
+                hover:bg-port-gore hover:text-white">
+                Kontakt
+            </button>
+        </nav>
+    </header>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '475px',
+      },
+      fontFamily: {
+        'galano-grotesque': ['Galano Grotesque', 'sans-serif'],
+      },
+      colors:{
+        'caribbean-green':'#00FFB0',
+        'port-gore':'#2B2161',
+        'dark-grey':'#4F4F4F',
+        'light-dark-grey':'#B7B7B7',
+        'lilac':'#5345AC'
+      }
+    },
   },
   plugins: [],
 }
