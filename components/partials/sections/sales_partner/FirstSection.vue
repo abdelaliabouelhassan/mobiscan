7cd3fb2998dd82898f7ee1e93e17ff74040001ab
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-09-05 13:59:17 +0100

    first section sales partner

---
+++ components/partials/sections/sales_partner/FirstSection.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class=" w-full">
+        <div class=" w-full bg-caribbean-green   rounded-[3.125rem] px-4 md:px-10 lg:px-24 pt-36 pb-32 md:pb-48 lg:pb-20 space-y-10 lg:space-y-16 lg:space-x-8 relative">
+            <h1 class=" text-port-gore text-4xl sm:text-6xl md:text-[5rem] font-galano-grotesque-medium font-normal">Vertriebspartner- Programm</h1>
+            <p class=" max-w-[37rem] w-full  text-port-gore text-xl md:text-3xl">
+            Lorem ipsum dolor sit amet consectetur. Enim quam porta tortor cursus adipiscing nunc tellus nunc sed.
+            </p>
+
+        <div
+            class=" bg-port-gore rounded-full md:h-[19.5625rem] md:w-[19.5625rem] h-[14rem] w-[14rem] flex absolute right-16 -bottom-32"
+            >
+            <img
+                src="/images/sales_partner/partner.png"
+                class="m-auto object-cover "
+                alt=""
+            />
+            </div>
+        </div>
+
+        <div class=" w-full pt-36  sm:pt-32  xl:pt-28 lg:pl-[7.5rem] pb-20 px-4">
+            <div class=" w-full max-w-[43.6875rem] flex flex-col items-start space-y-8">
+                  <h1 class=" text-port-gore text-[2.5rem] md:text-[3.125rem] font-galano-grotesque-medium  leading-[3.125rem]">Lassen Sie uns gemeinsam wachsen</h1>
+                  <span class=" text-port-gore font-galano-grotesque text-xl md:text-3xl font-normal">Lorem ipsum dolor sit amet consectetur. Morbi risus lacus lobortis nec.</span>
+                  <p class=" text-dark-grey font-normal text-lg md:text-xl">
+                    Lorem ipsum dolor sit amet consectetur. Viverra est cursus ac tellus egestas. Sollicitudin gravida quam ut cras laoreet. Diam libero aliquet ut nunc libero. Fringilla facilisis dictum euismod congue sodales a nulla scelerisque in. Placerat ipsum tincidunt scelerisque morbi ultrices.
+                    Lorem ipsum dolor sit amet consectetur. Viverra est cursus ac tellus egestas. Sollicitudin gravida quam ut cras laoreet. Diam libero aliquet ut nunc libero. Fringilla facilisis dictum euismod congue sodales a nulla scelerisque in. Placerat ipsum tincidunt scelerisque morbi ultrices.
+                    Diam libero aliquet ut nunc libero. Fringilla facilisis dictum euismod.
+                    Fringilla facilisis dictum euismod congue sodales a nulla scelerisque in. 
+                  </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+
+<style scoped>
+ .card-drop {
+    filter: drop-shadow(-8px -8px 24px rgba(0, 0, 0, 0.05));
+ }
+</style>
